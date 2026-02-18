2a29f1085ac5eb7fabd475d97a886338b3e010b8
“Testbit-9” <sspssp732@gmail.com>
2026-02-18 22:49:17 +0530

    Enable skipWaiting in PWA config

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,9 @@ import withPWAInit from 'next-pwa';
 
 const withPWA = withPWAInit({
     dest: 'public',
-    // disable: process.env.NODE_ENV === 'development',
+    skipWaiting: true,
+    register: true,
+    disable: process.env.NODE_ENV === 'development',
 });
 
 /** @type {import('next').NextConfig} */
